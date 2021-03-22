9d36abaa16daeb38f736d39ce47ee8df62d0fad8
ashishsamuel514 <60664601+ashishsamuel514@users.noreply.github.com>
2021-03-22 18:31:05 +0530

    Update Calculator.java

--- SimpleGitDemo/src/main/Calculator.java
+++ SimpleGitDemo/src/main/Calculator.java
@@ -9,6 +9,10 @@ public class Calculator {
 		      System.out.println(num1+num2);
 		      add(10,20);
 		  }
+	public static void add(int num1,int num2){
+		      System.out.println(num1-num2);
+		      add(10,20);
+		  }
 
 	
 
